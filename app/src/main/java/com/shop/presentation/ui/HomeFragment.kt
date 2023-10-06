33facb7a3fe02539a9c72424bc75cc2bd8c0749d
Anastasia Vilmovskaya <burberrry3@gmail.com>
2023-10-06 20:23:16 +0300

    Add main page of shop.

---
+++ app/src/main/java/com/shop/presentation/ui/HomeFragment.kt
@@ -0,0 +1,70 @@
+package com.shop.presentation.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.tabs.TabLayoutMediator
+import com.shop.R
+import com.shop.databinding.FragmentHomeBinding
+import com.shop.presentation.adapters.GenericRecyclerAdapter
+import com.shop.presentation.adapters.VpAdapter
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initBannerRecyclerView()
+
+
+        binding.vp.adapter = VpAdapter(this)
+        TabLayoutMediator(binding.tabLayout, binding.vp) { tab, position ->
+            if (position == 0) {
+                tab.text = "Guitars"
+            }
+            if (position == 1) {
+                tab.text = "Accessories"
+            }
+        }.attach()
+    }
+
+    private fun initBannerRecyclerView() {
+        val bindingInterface =
+            object : GenericRecyclerAdapter.GenericRecyclerBindingInterface<Int> {
+                override fun bindData(item: Int, view: View) {
+                    val imageView: ImageView = view.findViewById(R.id.banner_image)
+                    imageView.setImageResource(item)
+                }
+            }
+        binding.rvBanner.adapter = GenericRecyclerAdapter(
+            listOf(
+                R.drawable.banner_one,
+                R.drawable.banner_four,
+                R.drawable.banner_three
+            ),
+            R.layout.item_banner,
+            bindingInterface
+        )
+        binding.rvBanner.layoutManager = LinearLayoutManager(
+            context, LinearLayoutManager.HORIZONTAL, false
+        )
+    }
+}
