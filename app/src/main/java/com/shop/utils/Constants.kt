c726170725e620c0f3d66462302a699c39e485f9
Anastasia Vilmovskaya <burberrry3@gmail.com>
2023-11-01 17:35:54 +0300

    Room databases names

--- app/src/main/java/com/shop/utils/Constants.kt
+++ app/src/main/java/com/shop/utils/Constants.kt
@@ -2,4 +2,6 @@ package com.shop.utils
 
 object Constants {
     const val BASE_URL: String = "https://ktor-shop-backend.onrender.com"
+    const val DATABASE_NAME = "guitar_database"
+    const val SECOND_DATABASE_NAME = "accessories_database"
 }
