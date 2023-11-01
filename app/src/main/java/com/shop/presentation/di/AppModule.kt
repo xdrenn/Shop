db745e6fb18f6e8bee17fbea932747ce95201ac3
Anastasia Vilmovskaya <burberrry3@gmail.com>
2023-11-01 17:36:24 +0300

    Provide room databases and repositories

--- app/src/main/java/com/shop/presentation/di/AppModule.kt
+++ app/src/main/java/com/shop/presentation/di/AppModule.kt
@@ -3,9 +3,20 @@ package com.shop.presentation.di
 import android.app.Application
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import androidx.room.Room
 import com.shop.data.remote.ApiService
-import com.shop.data.repository.AuthRepository
-import com.shop.data.repository.AuthRepositoryImpl
+import com.shop.data.repository.AccessoryRepository
+import com.shop.data.repository.AccessoryRepositoryImpl
+import com.shop.data.repository.GuitarRepository
+import com.shop.data.repository.GuitarRepositoryImpl
+import com.shop.data.repository.UserRepository
+import com.shop.data.repository.UserRepositoryImpl
+import com.shop.room.accessories.AccessoriesDatabase
+import com.shop.room.accessories.AccessoriesRoomRepository
+import com.shop.room.accessories.AccessoriesRoomRepositoryImpl
+import com.shop.room.guitar.GuitarDatabase
+import com.shop.room.guitar.GuitarRoomRepository
+import com.shop.room.guitar.GuitarRoomRepositoryImpl
 import com.shop.utils.Constants
 import dagger.Module
 import dagger.Provides
@@ -50,15 +61,65 @@ class AppModule {
 
     @Singleton
     @Provides
-    fun provideSharedPref(app: Application): SharedPreferences{
+    fun provideSharedPref(app: Application): SharedPreferences {
         return app.getSharedPreferences("prefs", MODE_PRIVATE)
     }
+
     @Singleton
     @Provides
-    fun provideRepository(apiService: ApiService, preferences: SharedPreferences): AuthRepository
-    {
-        return AuthRepositoryImpl(apiService, preferences)
+    fun provideRepository(apiService: ApiService, preferences: SharedPreferences): UserRepository {
+        return UserRepositoryImpl(apiService, preferences)
     }
 
+    @Singleton
+    @Provides
+    fun provideGuitarRepository(
+        apiService: ApiService,
+        preferences: SharedPreferences
+    ): GuitarRepository {
+        return GuitarRepositoryImpl(apiService, preferences)
+    }
 
+    @Provides
+    @Singleton
+    fun provideDataBase(app: Application): GuitarDatabase {
+        return Room.databaseBuilder(
+            app,
+            GuitarDatabase::class.java,
+            Constants.DATABASE_NAME
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDbRepository(db: GuitarDatabase): GuitarRoomRepository {
+        return GuitarRoomRepositoryImpl(db.guitarDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAccessoriesRepository(
+        apiService: ApiService,
+        preferences: SharedPreferences
+    ): AccessoryRepository {
+        return AccessoryRepositoryImpl(apiService, preferences)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRoomDataBase(app: Application): AccessoriesDatabase {
+        return Room.databaseBuilder(
+            app,
+            AccessoriesDatabase::class.java,
+            Constants.SECOND_DATABASE_NAME
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideDatabaseRepository(db: AccessoriesDatabase): AccessoriesRoomRepository {
+        return AccessoriesRoomRepositoryImpl(db.accessoriesDao)
+    }
 }
