1bc174908b23175f73b84e9cd93a83fcf3304489
Anastasia Vilmovskaya <burberrry3@gmail.com>
2023-11-01 17:35:24 +0300

    New endpoints

--- app/src/main/java/com/shop/data/remote/ApiService.kt
+++ app/src/main/java/com/shop/data/remote/ApiService.kt
@@ -1,8 +1,17 @@
 package com.shop.data.remote
 
+import com.shop.data.model.AccessoriesList
+import com.shop.data.model.AccessoryRequestCategory
+import com.shop.data.model.AccessoryRequestId
+import com.shop.data.model.AccessoryRequestSubcategory
 import com.shop.data.model.AuthRequest
-import com.shop.data.model.Guitar
+import com.shop.data.model.DeleteRequest
+import com.shop.data.model.GuitarList
+import com.shop.data.model.GuitarRequestBrand
+import com.shop.data.model.GuitarRequestCategory
+import com.shop.data.model.GuitarRequestId
 import com.shop.data.model.TokenResponse
+import com.shop.data.model.UserList
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -12,6 +21,7 @@ import retrofit2.http.POST
 
 interface ApiService {
 
+    //user requests
     @Headers("Content-Type: application/json")
     @POST("api/signup")
     suspend fun signUp(@Body request: AuthRequest)
@@ -20,13 +30,56 @@ interface ApiService {
     @POST("api/login")
     suspend fun login(@Body request: AuthRequest): Response<TokenResponse>
 
+    @Headers("Content-Type: application/json")
+    @POST("api/delete")
+    suspend fun delete(@Header("Authorization") token: String, @Body request: DeleteRequest)
+
+    @Headers("Content-Type: application/json")
+    @POST("api/user")
+    suspend fun getUser(@Body request: AuthRequest): Response<UserList>
+
     @GET("authenticate")
     suspend fun authenticate(
         @Header("Authorization") token: String
     )
-    @GET("api/guitar")
-    suspend fun getGuitars(
-        @Header("Authorization") token: String
-    ): Response<Guitar>
+
+    //guitar requests
+    @POST("api/guitar/category")
+    suspend fun getGuitarByCategory(
+        @Header("Authorization") token: String,
+        @Body request: GuitarRequestCategory
+    ): Response<GuitarList>
+
+    @POST("api/guitar/brand")
+    suspend fun getGuitarByBrand(
+        @Header("Authorization") token: String,
+        @Body request: GuitarRequestBrand
+    ): Response<GuitarList>
+
+    @POST("api/guitar/id")
+    suspend fun getGuitarById(
+        @Header("Authorization") token: String,
+        @Body request: GuitarRequestId
+    ): Response<GuitarList>
+
+    //accessories request
+    @POST("api/accessory/category")
+    suspend fun getAccessoryByCategory(
+        @Header("Authorization") token: String,
+        @Body request: AccessoryRequestCategory
+    ): Response<AccessoriesList>
+
+    @POST("api/accessory/id")
+    suspend fun getAccessoryById(
+        @Header("Authorization") token: String,
+        @Body request: AccessoryRequestId
+    ): Response<AccessoriesList>
+
+    @POST("api/accessory/subcategory")
+    suspend fun getAccessoryBySubcategory(
+        @Header("Authorization") token: String,
+        @Body request: AccessoryRequestSubcategory
+    ): Response<AccessoriesList>
+
 
 }
