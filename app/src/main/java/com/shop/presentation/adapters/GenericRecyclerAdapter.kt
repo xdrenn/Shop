41445339619c660456fdbe8ee25a11cadaf56cd8
Anastasia Vilmovskaya <burberrry3@gmail.com>
2023-11-01 17:05:39 +0300

    Change from RecyclerAdapter to ListAdapter and add clickListener

--- app/src/main/java/com/shop/presentation/adapters/GenericRecyclerAdapter.kt
+++ app/src/main/java/com/shop/presentation/adapters/GenericRecyclerAdapter.kt
@@ -1,26 +1,44 @@
 package com.shop.presentation.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.shop.utils.GenericClickListener
 
 class GenericRecyclerAdapter<T : Any>(
-    private val dataList: List<T>, @LayoutRes val layoutID: Int,
-    private val bindingInterface: GenericRecyclerBindingInterface<T>
+    @LayoutRes val layoutID: Int,
+    private val bindingInterface: GenericRecyclerBindingInterface<T>,
+    private var clickListener: GenericClickListener<T>? = null
 ) :
-    RecyclerView.Adapter<GenericRecyclerAdapter.ViewHolder>() {
+    ListAdapter<T, GenericRecyclerAdapter.ViewHolder>(GenericDiffUtilCallback()) {
 
     interface GenericRecyclerBindingInterface<T> {
-        fun bindData(item: T, view: View)
+        fun bindData(
+            item: T, view: View, clickListener: GenericClickListener<T>?,
+            position: Int
+        )
     }
 
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         fun <T : Any> bind(
             item: T,
-            bindingInterface: GenericRecyclerBindingInterface<T>
-        ) = bindingInterface.bindData(item, view)
+            bindingInterface: GenericRecyclerBindingInterface<T>,
+            clickListener: GenericClickListener<T>?,
+            position: Int,
+        ) {
+            bindingInterface.bindData(item, view, clickListener, position)
+
+            clickListener?.let {
+                view.setOnClickListener {
+                    clickListener.onClick(position, item)
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,10 +46,22 @@ class GenericRecyclerAdapter<T : Any>(
         return ViewHolder(view)
     }
 
-    override fun getItemCount(): Int = dataList.size
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = dataList[position]
-        holder.bind(item, bindingInterface)
+        holder.bind(getItem(position), bindingInterface, clickListener, position)
+    }
+
+    class GenericDiffUtilCallback<T : Any> : DiffUtil.ItemCallback<T>() {
+        override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
+            return oldItem == newItem
+        }
+
+        @SuppressLint("DiffUtilEquals")
+        override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    fun setOnClickListener(genericClickListener: GenericClickListener<T>) {
+        this.clickListener = genericClickListener
     }
 }
